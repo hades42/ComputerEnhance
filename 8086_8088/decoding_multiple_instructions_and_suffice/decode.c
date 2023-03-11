34c09b244b3a7b6dd8b42bf5e63c5389ecc5a6b3
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2023-03-12 01:26:59 +1100

    decode REGISTER_TO_REGISTER

--- 8086_8088/decoding_multiple_instructions_and_suffice/decode.c
+++ 8086_8088/decoding_multiple_instructions_and_suffice/decode.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define IMM_TO_REGISTER 0b1011 // 1011
+#define IMM_TO_REGISTER 0b1011
+#define REGISTER_TO_REGISTER 0b100010
 
 char *registers[2][8] = {{"al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"},
                          {"ax", "cx", "dx", "bx", "sp", "bp", "si", "di"}};
@@ -17,20 +18,38 @@ int main() {
       // First Byte
       buffer = fgetc(asmFile);
 
-      // Check for Immediate To Register
+      // Decode Immediate To Register
       if (buffer >> 4 == IMM_TO_REGISTER) {
         bool w = buffer >> 3 & 1;
         uint8_t reg = buffer & 0b111;
         // Word
         if (w) {
+          // Want to read signed number because there are negative numbers
           int16_t data;
           fread(&data, sizeof(int16_t), 1, asmFile);
-          printf("%s %d\n", registers[w][reg], data);
+          printf("mov %s, %d\n", registers[w][reg], data);
         } else {
           // byte
           int8_t data;
           fread(&data, sizeof(int8_t), 1, asmFile);
-          printf("%s %d\n", registers[w][reg], data);
+          printf("mov %s, %d\n", registers[w][reg], data);
+        }
+      } else if (buffer >> 2 == REGISTER_TO_REGISTER) {
+        // Decode REGISTER_TO_REGISTER
+        bool w = buffer & 1;
+        bool d = buffer >> 1 & 1;
+
+        uint8_t nextBuffer;
+        fread(&nextBuffer, sizeof(uint8_t), 1, asmFile);
+
+        uint8_t mod = nextBuffer >> 6;
+        uint8_t reg = nextBuffer >> 3 & 0b111;
+        uint8_t r_m = nextBuffer & 0b111;
+
+        if (d) {
+          printf("mov %s, %s \n", registers[w][reg], registers[w][r_m]);
+        } else {
+          printf("mov %s, %s \n", registers[w][r_m], registers[w][reg]);
         }
       }
     }
--- 8086_8088/decoding_multiple_instructions_and_suffice/test.asm
+++ 8086_8088/decoding_multiple_instructions_and_suffice/test.asm
@@ -1,5 +1,9 @@
 bits 16
 
+; Register-to-register
+mov si, bx
+mov dh, al
+
 ; 8-bit immediate-to-register
 mov cl, 12
 mov ch, -12
